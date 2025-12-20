d3a03b3ba59fc38918431b317c84c4389b2c13eb
thakkaralpesh <thakkaralpesh@gmail.com>
2025-12-20 11:28:36 +0530

    New Changes (Dto,automapper,profile)

---
+++ PlatformService/Data/Prepdb.cs
@@ -0,0 +1,33 @@
+﻿namespace PlatformService.Data
+{
+    public static class Prepdb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceSope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceSope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if(!context.Platforms.Any())
+            {
+                Console.WriteLine("--> Seeding Data...");
+
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                    new Models.Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                    new Models.Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+
+                context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("--> We already have data...");
+            }
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,8 +7,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
    opt.UseInMemoryDatabase("InMem"); 
 });
 
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 var app = builder.Build();
+Prepdb.PrepPopulation(app);
 app.UseHttpsRedirection();
 app.Run();
