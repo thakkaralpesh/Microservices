55135e32b5fe8e30bdbcd9087d0345ddcf6dbf7b
Alpesh Thakkar <thakkaralpesh100@gmail.com>
2025-12-25 10:38:46 +0530

    publish

--- PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net10.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711cbaa97872ef2e32bf2c6d013424c3d4455953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f3830ca19f740b06bd09f9e576ad883ca2cf99")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
